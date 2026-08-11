1e9967a64cccbe90da5ab7a66c2e4a75096c4b9c
jade <jade@hopepunk.me>
2026-08-11 15:35:20 +1000

    suppress error that isn't actually a problem

--- crates/libkiwix-rust/src/bridge.cc
+++ crates/libkiwix-rust/src/bridge.cc
@@ -1,5 +1,7 @@
-#include "src/bridge.h"
+#include "bridge.h"
+#pragma diag_suppress 1696
 #include "libkiwix-rust/src/lib.rs.h"
+#pragma diag_default 1696
 
 std::shared_ptr<kiwix::Library> create_library() {
     return kiwix::Library::create();
