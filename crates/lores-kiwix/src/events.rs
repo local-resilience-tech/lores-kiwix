a7eef0b874d6a8d3b7a98fbfa3c5688744134ab3
jade <jade@hopepunk.me>
2026-08-25 13:08:57 +0100

    debug output

--- crates/lores-kiwix/src/events.rs
+++ crates/lores-kiwix/src/events.rs
@@ -9,13 +9,17 @@ pub fn register_event_handlers(node: &LoresKiwixNode, pool: SqlitePool) {
     tokio::spawn(async move {
         loop {
             match rx.recv().await {
-                Ok(node_op) => match node_op.op {
-                    AppOperation::ZimRegisteredV1(data) => {
-                        if let Err(err) = zims::insert_zim(&pool, &data).await {
-                            tracing::error!(error = %err, "Failed to insert ZIM into projection");
+                Ok(node_op) => {
+                    println!("processing op: {:?}", node_op);
+
+                    match node_op.op {
+                        AppOperation::ZimRegisteredV1(data) => {
+                            if let Err(err) = zims::insert_zim(&pool, &data).await {
+                                tracing::error!(error = %err, "Failed to insert ZIM into projection");
+                            }
                         }
                     }
-                },
+                }
                 Err(tokio::sync::broadcast::error::RecvError::Lagged(n)) => {
                     tracing::warn!(skipped = n, "Event handler lagged");
                 }
